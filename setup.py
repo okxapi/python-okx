e2b9f388e94f2b213477db12cf9d7d6c7f04be6b
shiyao.zhu <shiyao.zhu@okg.com>
2023-02-16 15:47:56 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+import okx
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-okx",
+    version=okx.__version__,
+    author="okxv5api",
+    author_email="api@okg.com",
+    description="Python SDK for OKX",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://okx.com/docs-v5/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "importlib-metadata",
+        "keyring",
+        "requests",
+        "Twisted",
+        "pyOpenSSL"
+    ]
+)
