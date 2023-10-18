7cf0ca5cd39c21d1e1a113e4e26c4a476a49716b
zihao.jiang <zihao.jiang@okg.com>
2023-10-18 10:46:12 +0800

    fix

--- okx/SpreadTrading.py
+++ okx/SpreadTrading.py
@@ -2,7 +2,7 @@ from .client import Client
 from .consts import *
 
 
-class SpreadAPI(Client):
+class SpreadTradingAPI(Client):
 
     def __init__(self, api_key='-1', api_secret_key='-1', passphrase='-1', use_server_time=False, flag='1', domain = 'https://www.okx.com',debug = True):
         Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, flag, domain,debug)
--- okx/__init__.py
+++ okx/__init__.py
@@ -2,4 +2,4 @@
 Python SDK for the OKX API v5
 
 """
-__version__="0.2.2"
+__version__="0.2.3"
