c295296a64d40128427e706320905ae6c949aff7
LeRaymy <remi.schlosser@gmail.com>
2024-04-30 18:52:48 +0200

    fix: use f-string

--- okx/okxclient.py
+++ okx/okxclient.py
@@ -34,8 +34,8 @@ class OkxClient(Client):
             header = utils.get_header_no_sign(self.flag, self.debug)
         response = None
         if self.debug == True:
-            logger.debug('domain:',self.domain)
-            logger.debug('url:',request_path)
+            logger.debug(f'domain: {self.domain}')
+            logger.debug(f'url: {request_path}')
         if method == c.GET:
             response = self.get(request_path, headers=header)
         elif method == c.POST:
--- okx/utils.py
+++ okx/utils.py
@@ -14,7 +14,7 @@ def sign(message, secretKey):
 
 def pre_hash(timestamp, method, request_path, body,debug = True):
     if debug == True:
-        logger.debug('body: ',body)
+        logger.debug(f'body: {body}')
     return str(timestamp) + str.upper(method) + request_path + body
 
 
@@ -27,7 +27,7 @@ def get_header(api_key, sign, timestamp, passphrase, flag,debug = True):
     header[c.OK_ACCESS_PASSPHRASE] = passphrase
     header['x-simulated-trading'] = flag
     if debug == True:
-        logger.debug('header: ',header)
+        logger.debug(f'header: {header}')
     return header
 
 def get_header_no_sign(flag,debug = True):
@@ -35,7 +35,7 @@ def get_header_no_sign(flag,debug = True):
     header[c.CONTENT_TYPE] = c.APPLICATION_JSON
     header['x-simulated-trading'] = flag
     if debug == True:
-        logger.debug('header: ',header)
+        logger.debug(f'header: {header}')
     return header
 
 def parse_params_to_str(params):
@@ -44,7 +44,7 @@ def parse_params_to_str(params):
         if(value != ''):
             url = url + str(key) + '=' + str(value) + '&'
     url = url[0:-1]
-    #logger.debug('url:',url)
+    #logger.debug(f'url: {url}')
     return url
 
 
